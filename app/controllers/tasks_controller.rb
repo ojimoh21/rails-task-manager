2ba3b0d73df801e03134a49847193329a4e0b3c8
muinat jimoh <ojimoh21@live.co.uk>
2021-11-09 18:20:08 +0000

    task manager completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,40 @@
 class TasksController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :delete]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def delete
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def find_restaurant
+    @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- db/migrate/20211109121315_create_tasks.rb
+++ db/migrate/20211109121315_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :details
+      t.text :details
       t.boolean :completed, default: false
       t.timestamps
     end
